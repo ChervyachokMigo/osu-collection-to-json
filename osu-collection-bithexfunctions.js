0f5bb2f0d7f3d7b7aa52214e6a6e13de348cf125
cpu_1 <74270643+ChervyachokMigo@users.noreply.github.com>
2021-12-05 10:10:18 +0500

    Add files via upload

--- osu-collection-bithexfunctions.js
+++ osu-collection-bithexfunctions.js
@@ -1,26 +1,129 @@
 var log = console.log.bind(console)
+var fs = require('fs')
 module.exports = {
 	_cursoroffset: 0,
 	debug: 0,
 	fd: 0,
-	getInt: function(hex){
-		return this.bin2int(this.hex2bin(this.changeEndianness(this.buffer2hexstr(hex))))
+	dataBuffer: Buffer.alloc(0),
+
+	openFileDB: async function(fileDb){
+		var stats = await fs.statSync(fileDb)
+		var fileSizeInBytes = stats.size
+		this.dataBuffer = Buffer.alloc(fileSizeInBytes)
+		this.fd = await fs.promises.open(fileDb,'r')
+		this._cursoroffset = 0
+		//throw new Error(this.dataBuffer.length)
+	},
+
+	closeFileDB: async function(fileDb){
+		await this.fd.close()
+	},
+
+	getByte: async function (){
+		return this.getInt8()
+	},
+
+	getBool: async function (){
+		return this.getInt8()
+	},
+
+	getShort: async function (){
+		return this.getInt16()
+	},
+
+	getInt: async function (){
+		return this.getInt32()
+	},
+
+	getDate: async function (){
+		return this.getInt64()
+	},
+
+	getLong: async function (){
+		return this.getInt64()
+	},
+
+	getSingle: async function(){
+		let _cursoroffset_old = this._cursoroffset
+		this._cursoroffset += 4
+		let res = await this.bufferRead(_cursoroffset_old,4)
+		res=res.readInt32LE(0).toString(16)
+		res = res.substring(1)
+		res = Buffer.from(res,'hex')
+		res = (res.readInt16LE(0).toString(10))/2
+		if (this.debug==1) log (res)
+		return res
+	},
+
+	getIntDoublePair: async function(){
+		let isDebug = this.debug
+		if (this.debug == 1) this.debug = 0
+
+		let res = []
+
+		res.count = await this.getInt()
+		
+		for (let i=1;i<=res.count;i++){
+			let res2 = {}
+			res2.byte1 = await this.getByte()
+			res2.int1 = await this.getInt()
+			res2.byte2 = await this.getByte()
+			res2.starrating = await this.getDouble()
+			res.push(res2)
+		}
+
+		if (isDebug == 1) this.debug = 1
+		if (this.debug==1) {
+			log (res)
+		}
+		return res
+	},
+
+	getTimingPoint: async function(){
+		let isDebug = this.debug
+		if (this.debug == 1) this.debug = 0
+		let res = {}
+		res.double1 = await this.getDouble()
+		res.double2 = await this.getDouble()
+		res.bool1 = await this.getBool()
+		if (isDebug == 1) this.debug = 1
+		if (this.debug==1) log (res)
+		return res
+	},
+
+	getDouble: async function (){
+		return this.getInt64()
+	},
+
+	buff2int: async function(hex){
+		return await this.bin2int(await this.hex2bin(await this.changeEndianness(await this.buffer2hexstr(hex))))
 	},
 
 	getString: async function(){
-		let stringCode = await this.getInt4()
+		let isDebug = this.debug
+		if (this.debug == 1) this.debug = 0
+		let stringCode = await this.getInt8()
 		if (stringCode == 11){
-			let stringLength = await this.getInt4()
+			let stringLength = await this.getInt8()
 			let res = (await this.getStringBytes(stringLength)).toString('utf8')
-			if (this.debug==1) log (res)
+			if (isDebug == 1) this.debug = 1
+			if (this.debug==1) log ({String: res,Length:stringLength})
 			return res
 		} else {
-			log ('error read string')
-			return false
+			let stringCode = await this.getInt8()
+			if (stringCode == 11){
+				let stringLength = await this.getInt8()
+				let res = (await this.getStringBytes(stringLength)).toString('utf8')
+				if (isDebug == 1) this.debug = 1
+				if (this.debug==1) log ({String: res,Length:stringLength})
+				return res
+		}
+			//log ('error read string')
+			//return false
 		}
 	},
 
-	changeEndianness: function (string){
+	changeEndianness: async function (string){
 	  const result = [];
 	  let len = string.length - 2;
 	  while (len >= 0) {
@@ -30,7 +133,7 @@ module.exports = {
 	  return result.join('');
 	},
 
-	 hex2bin: function (hex){
+	 hex2bin: async function (hex){
 	  hex = hex.replace("0x", "").toLowerCase();
 	  var out = "";
 	  for(var c of hex) {
@@ -65,45 +168,65 @@ module.exports = {
 
 	getStringBytes: async function (length){
 		let _cursoroffset_old = this._cursoroffset
-		this._cursoroffset += length
+		if (length > 0){
+			let stringType = await this.getInt8withoutOffset(await this.bufferRead(_cursoroffset_old,1))
+			if (stringType == 1) {
+				log ('type '+stringType)
+				this._cursoroffset += 1
+				this._cursoroffset += length	
+				let res2 = await this.bufferRead(_cursoroffset_old,length)
+
+			} else {
+				this._cursoroffset += length	
+			}
+		} else {
+			this._cursoroffset += length
+		}
 		let res = await this.bufferRead(_cursoroffset_old,length)
-		//if (debug==1) log (res)
+			
+		
+		return res
+	},
+
+	getInt64: async function (){
+		let _cursoroffset_old = this._cursoroffset
+		this._cursoroffset += 8
+		let res = await this.buff2int(await this.bufferRead(_cursoroffset_old,8))
+		if (this.debug==1) log (res)
 		return res
 	},
 
 	getInt32: async function (){
 		let _cursoroffset_old = this._cursoroffset
-		this._cursoroffset += 8
-		let res = this.getInt(await this.bufferRead(_cursoroffset_old,8))
+		this._cursoroffset += 4
+		let res = await this.buff2int(await this.bufferRead(_cursoroffset_old,4))
 		if (this.debug==1) log (res)
 		return res
 	},
 
 	getInt16: async function (){
 		let _cursoroffset_old = this._cursoroffset
-		this._cursoroffset += 4
-		let res = this.getInt(await this.bufferRead(_cursoroffset_old,4))
+		this._cursoroffset += 2
+		let res = await this.buff2int(await this.bufferRead(_cursoroffset_old,2))
 		if (this.debug==1) log (res)
 		return res
 	},
 
 	getInt8: async function (){
 		let _cursoroffset_old = this._cursoroffset
-		this._cursoroffset += 2
-		let res = this.getInt(await this.bufferRead(_cursoroffset_old,2))
-		if (this.debug==1) log (res)
-		return res
-	},
-
-	getInt4: async function (){
-		let _cursoroffset_old = this._cursoroffset
 		this._cursoroffset += 1
-		let res = this.getInt(await this.bufferRead(_cursoroffset_old,1))
+		let res = await this.buff2int(await this.bufferRead(_cursoroffset_old,1))
 		if (this.debug==1) log (res)
 		return res
 	},
 
-	bin2int: function(binary){
+	getInt8withoutOffset: async function (){
+		let res = await this.buff2int(await this.bufferRead(this._cursoroffset,1))
+		if (this.debug==1) log (res)
+		return res
+	},
+
+	bin2int: async function(binary){
 		return parseInt(binary,2)
 	},
 
@@ -111,11 +234,11 @@ module.exports = {
 		return parseInt(hex,16)
 	},
 
-	int2hex: function (int){
+	int2hex: async function (int){
 		return int.toString(16)
 	},
 
-	buffer2hexstr: function (BufferHex){
+	buffer2hexstr: async function (BufferHex){
 		return BufferHex.toString('hex')
 	}
 }
--- osu-collection-reader.js
+++ osu-collection-reader.js
@@ -3,33 +3,28 @@ var fs = require('fs')
 var path = require('path')
 
 var bh = require('./osu-collection-bithexfunctions.js')
-bh.debug = 0
+bh.debug = 1
 
 var collectionReader = {
 
-dataBuffer: Buffer.alloc(0),
-
 data: [],
+beatmaps: [],
 
-run: async function(){
+readCollectionDb: async function(){
 
 	var collectionfile = 'C:\\Osu\\collection.db'
 	var collectionsJson = 'collections.json'
 
-	var stats = await fs.statSync(collectionfile)
-	var fileSizeInBytes = stats.size
-	this.dataBuffer = Buffer.alloc(fileSizeInBytes)
+	await bh.openFileDB(collectionfile)	
 
-	bh.fd = await fs.promises.open(collectionfile,'r')
-
-	this.data.version = await bh.getInt16()
-	this.data.collectionsLength = await bh.getInt16()
+	this.data.version = await bh.getInt()
+	this.data.collectionsLength = await bh.getInt()
 	this.data.collections = []
 
 	for (let cc = 1; cc <= this.data.collectionsLength; cc++){
 
 		let collectionName = await bh.getString()
-		let collectionCount = await bh.getInt16()
+		let collectionCount = await bh.getInt()
 		let hashes = []
 
 		for (let i = 1; i<= collectionCount; i++){
@@ -41,17 +36,141 @@ run: async function(){
 
 	}
 
-	bh.fd.close()
+	await bh.closeFileDB()
 
-	log (this.data)
 	var collectionsJsonData = await JSON.stringify({ ...this.data})
 	var colJsonFile = await fs.promises.open(collectionsJson,'w')
 	await colJsonFile.writeFile(collectionsJsonData)
 	await colJsonFile.close()
+},
+
+readOsuDb: async function(){
+
+	var osuDbFile = 'C:\\Osu\\osu!.db'
+
+	await bh.openFileDB(osuDbFile)
+
+	this.beatmaps.version = await bh.getInt()
+
+	this.beatmaps.FolderCount = await bh.getInt()
+
+	this.beatmaps.isAcountUnlocked = await bh.getBool()
+
+	this.beatmaps.DateWillUnlocked = await bh.getDate()
+
+	this.beatmaps.PlayerName = await bh.getString()
+
+	this.beatmaps.NumberBeatmaps = await bh.getInt()
+
+	this.beatmaps.beatmaps = []
+
+	for (let nb = 1; nb <= this.beatmaps.NumberBeatmaps; nb++){
+		try{
+		let beatmap = {}
+		beatmap.artist = await bh.getString()
+
+		beatmap.artistUni = await bh.getString()
+
+		beatmap.title = await bh.getString()
+
+		beatmap.titleUni = await bh.getString()
+
+		beatmap.creator = await bh.getString()
+
+		beatmap.difficulty = await bh.getString()
+
+		beatmap.audioFile = await bh.getString()
+
+		beatmap.hash = await bh.getString()
+
+		beatmap.osuFilename = await bh.getString()
+
+		beatmap.ranked = await bh.getByte()
+
+		beatmap.hitcircles = await bh.getShort()
+
+		beatmap.sliders = await bh.getShort()
+
+		beatmap.spinners = await bh.getShort()
+
+		beatmap.lastModify = await bh.getLong()
+
+		beatmap.AR = await bh.getInt()//await bh.getSingle()
+
+		beatmap.CS = await bh.getInt()//await bh.getSingle()
+
+		beatmap.HP = await bh.getInt()//await bh.getSingle()
+
+		beatmap.OD = await bh.getInt()//await bh.getSingle()
+
+		beatmap.sliderVelocity = await bh.getDouble()
+
+		beatmap.stars = await bh.getIntDoublePair()
+		beatmap.stars2 = await bh.getIntDoublePair()
+		beatmap.stars3 = await bh.getIntDoublePair()
+		beatmap.stars4 = await bh.getIntDoublePair()
+
+		beatmap.draintimeSec = await bh.getInt()
+
+		beatmap.draintimeMs = await bh.getInt()
+
+		beatmap.audioPreviewTime = await bh.getInt()
+
+		beatmap.timingPointsNumber = await bh.getInt()
+
+		beatmap.timingPoints = []
+
+		for (let tp=1; tp<=beatmap.timingPointsNumber; tp++){
+			beatmap.timingPoints.push(await bh.getTimingPoint())
+		}
+
+		beatmap.Id = await bh.getInt()
+		beatmap.setId= await bh.getInt()
+		beatmap.threadId = await bh.getInt()
+
+		beatmap.gradeStd = await bh.getByte()
+		beatmap.gradeTaiko = await bh.getByte()
+		beatmap.gradeCTB = await bh.getByte()
+		beatmap.gradeMania = await bh.getByte()
+
+		beatmap.localOffset = await bh.getShort()
+		beatmap.StackLaniency = await bh.getInt()
+
+		beatmap.gamemode = await bh.getByte()
+
+		beatmap.source = await bh.getString()
+		beatmap.tags = await bh.getString()
+
+		beatmap.onlineOffset = await bh.getShort()
+		beatmap.fontTitle = await bh.getString()
+
+		isBeatmapPlayed = await bh.getBool()
+		beatmap.lastTimePlayed = await bh.getLong()
+		isBeatmapOsz2 = await bh.getBool()
+
+		beatmap.folderName = await bh.getString()
+		beatmap.lastTimeChecked = await bh.getLong()
+
+		isIgnoreSounds = await bh.getBool()
+		isIgnoreSkin = await bh.getBool()
+		isDisableStoryboard = await bh.getBool()
+		isDisableVideo = await bh.getBool()
+		isVisualOverride = await bh.getBool()
+
+		beatmap.UnknownInt = await bh.getInt()
+		beatmap.ManiaScrollSpeed = await bh.getByte()
+		} catch (e){
+	log (e)
+}
+	}
+
+
+	await bh.closeFileDB()
+
 }}
 
 
 main = async function(){
-	return (await collectionReader.run())
+	return (await collectionReader.readOsuDb())
 }
 main()
